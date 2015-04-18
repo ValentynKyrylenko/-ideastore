87ed9a5c4d9f7aaf44439dc496e3a2f6611fcac9
unknown <valentynmail@gmail.com>
2015-04-19 02:10:46 +0300

    normalized css

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss','resources/css');
     mix.styles([
+        'libs/normalize.css',
         'libs/bootstrap.min.css',
         'libs/select2.min.css',
         'app.css'
