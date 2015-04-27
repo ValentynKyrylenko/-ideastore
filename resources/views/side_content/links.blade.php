cf6c0289160c414b7e47a58aa8c7486e1b4dc66c
unknown <valentynmail@gmail.com>
2015-04-27 22:06:56 +0300

    home copy

--- resources/views/side_content/links.blade.php
+++ resources/views/side_content/links.blade.php
@@ -2,5 +2,5 @@
      <div class="panel-heading">
          <i class="fa fa-smile-o fa-2x" class="panel-title">  Партнёры проекта</i>
      </div>
-     <div class="panel-body">Здесь будут ссылки или баннеры на партнёрские сайты.</div>
+     <div class="panel-body paragraph">Здесь будут ссылки или баннеры на партнёрские сайты.</div>
  </div></div> <!--3 Block END-->
