e46e887d6fba5903d5e1765b5284c103b17c2d7e
unknown <valentynmail@gmail.com>
2015-04-27 22:52:42 +0300

    corrected slick carousel at home

--- resources/views/side_content/slick.blade.php
+++ resources/views/side_content/slick.blade.php
@@ -1,9 +1,8 @@
   <div class="slick_slide hidden-sm hidden-xs">
-    <div><img src="http://localhost/laravel_1/public/media/banners/banner-site-mini.jpg"></div>
-    <div><img src="http://localhost/laravel_1/public/media/banners/dosug.jpg"></div>
-    <div><img src="http://localhost/laravel_1/public/media/banners/idei-dlya-tvor.jpg"></div>
-    <div><img src="http://localhost/laravel_1/public/media/banners/magazin.jpg"></div>
-    <div><img src="http://localhost/laravel_1/public/media/banners/sammer-2014.jpg"></div>
+    <div><img src="http://localhost/laravel_1/public/media/banner/banner-site-mini.jpg"></div>
+    <div><img src="http://localhost/laravel_1/public/media/banner/dosug.jpg"></div>
+    <div><img src="http://localhost/laravel_1/public/media/banner/idei-dlya-tvor.jpg"></div>
+    <div><img src="http://localhost/laravel_1/public/media/banner/sammer-2014.jpg"></div>
   </div>
 
 
