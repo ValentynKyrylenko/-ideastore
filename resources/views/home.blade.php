1d34ab18d006ea4527c3bfa6af44b11b11dc667e
Valentyn <valentynmail@gmail.com>
2015-05-06 11:26:46 +0300

    fixed content 06_05_15

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,9 @@
 @extends('app')
 @section('articles')
 {!! Breadcrumbs::render('home') !!}
-
                            <div class="row">
                              <div class="col-md-8 col-md-offset-2">
                                 {!! HTML::image('http://localhost/laravel_1/public/media/background_images/ideastore-vdohnovlyaet.jpg', 'a picture', array('class' => 'home_page_bg_image static hidden-sm hidden-xs')) !!}
                              </div>
                            </div>
-
-
 @endsection
