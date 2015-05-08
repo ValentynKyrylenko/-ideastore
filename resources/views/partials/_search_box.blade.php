19c25b9b31930b410dcebe49e33adc0cea060c68
Valentyn <valentynmail@gmail.com>
2015-05-08 16:48:40 +0300

    final before 9 may

--- resources/views/partials/_search_box.blade.php
+++ resources/views/partials/_search_box.blade.php
@@ -1,10 +1,10 @@
-<h1 class="h1">Ищи своё вдохновение!</h1>
+<h1 class="h1"></h1>
 
 <div class="flexsearch">
 		<div class="flexsearch--wrapper">
 			<form class="flexsearch--form" action="#" method="post">
 				<div class="flexsearch--input-wrapper">
-					<input class="flexsearch--input" type="search" placeholder="искать...">
+					<input class="flexsearch--input" type="search" placeholder="Искать своё вдохновение...">
 				</div>
 				<input class="flexsearch--submit" type="submit" value="&#10140;"/>
 			</form>
