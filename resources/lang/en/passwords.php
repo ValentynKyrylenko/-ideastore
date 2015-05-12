935dfe58c05f45fa34b159baa88a468430f6ca36
Valentyn <valentynmail@gmail.com>
2015-05-12 12:47:00 +0300

    validation in russian lenguage

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -2,21 +2,25 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Языковые ресурсы напоминания пароля
+    |--------------------------------------------------------------------------
+    |
+    | Последующие языковые строки возвращаются брокером паролей на неудачные
+    | попытки обновления пароля в таких случаях, как ошибочный код сброса
+    | пароля или неверный новый пароль.
+    |
+    */
 
-	"password" => "Passwords must be at least six characters and match the confirmation.",
-	"user" => "We can't find a user with that e-mail address.",
-	"token" => "This password reset token is invalid.",
-	"sent" => "We have e-mailed your password reset link!",
-	"reset" => "Your password has been reset!",
+    "password" => "Пароль должен быть не менее шести символов и совпадать с подтверждением.",
+
+    "user"     => "Не удалось найти пользователя с указанным электронным адресом.",
+
+    "token"    => "Ошибочный код сброса пароля.",
+
+    "sent"     => "Ссылка на сброс пароля была отправлена!",
+
+    "reset"    => "Пароль был сброшен!",
 
 ];
