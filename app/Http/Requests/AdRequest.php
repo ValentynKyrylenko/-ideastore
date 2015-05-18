c7e69588f6de7ff6f090fea0f5f698ff28676ab1
Valentyn <valentynmail@gmail.com>
2015-05-18 16:09:23 +0300

    regex final fix

--- app/Http/Requests/AdRequest.php
+++ app/Http/Requests/AdRequest.php
@@ -24,9 +24,9 @@ class AdRequest extends Request {
 		return [
             'published_at' => 'required|date',
             'title' => 'required|min:2|max:50|string|regex:/^[\pL\s\d\.\,\!\-\:]+$/u',
-            'body' => 'required|min:5|max:300|regex:/^[\pL\s\d]+$/u',
+            'body' => 'required|min:5|max:300|regex:/^[\pL\s\d\.\,\!\-\:]+$/u',
             'image'=>'image|mimes:jpeg,jpg,bmp,png,gif',
-            'location' => 'required|min:2|regex:/^[\pL\s\d]+$/u',
+            'location' => 'min:2|regex:/^[\pL\s\d\.\,\!\-\:]+$/u',
             'phone' => 'required|digits_between:7,10',
             'price' => 'regex:/^\d*(\.\d{2})?$/'
 		];
