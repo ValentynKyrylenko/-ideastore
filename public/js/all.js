93231c2a2f3e340b8433b369e421f2c53c39aee1
unknown <valentynmail@gmail.com>
2015-05-25 22:15:43 +0300

    js not fixed home

--- public/js/all.js
+++ public/js/all.js
@@ -11771,14 +11771,67 @@ $(document).ready(function() {
 
     $('select').select2();
 
-    $('#accordion').accordion();/** Jquery UI**/
-
 
     $('#data_table').DataTable();/** DataTables**/
 
-        // handle jQuery plugin naming conflict between jQuery UI and Bootstrap
-    $.widget.bridge('uibutton', $.ui.button);
-    $.widget.bridge('uitooltip', $.ui.tooltip);
+    //------------------------------------------------------Carusel---------------------------------------//
+    var topoffset = 50; //menu height
+    var slideqty = $('#featured .item').length;
+    var wheight = $(window).height();//get the height of the window
+    var randSlide = Math.floor(Math.random()*slideqty);
+
+
+    $('#featured .item').eq(randSlide).addClass('active');
+
+    $('.fullheight').css('height', wheight);//set to window tallness
+
+    //replvace IMG inside carusel with a background image
+    $('#featured .item img').each(function(){
+        var imgSrc = $(this).attr('src');
+        $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+        $(this).remove();
+    });
+
+    //adjust height of .fullheight elements on window resize
+    $(window).resize(function(){
+        wheight = $(window).height()//get the height of the window
+        $('.fullheight').css('height', wheight);
+    });
+
+//                   //Activate Scrollpy
+//                   $('body').scrollspy({
+//                   target:'header .navbar',
+//                   offset: topoffset
+//                   });
+//
+    //Add inbody class
+    var hash =$(this).find('li .active a').attr('href');
+    if(hash !=='#featured') {
+        $('header nav').addClass('inbody');
+    } else {
+        $('header nav').removeClass('inbody');
+    }
+    //Add an inbody class to nav when scrollspy event fires
+    //
+    //
+    //
+
+    //Automatically generate corousel indicators
+    for (var i=0; i<slideqty; i++) {
+        var insertText = '<li data-target="#featured" data-slide-to="'+ i + '"';
+        if (i === randSlide){
+            insertText += 'class="active" ';
+        }
+        insertText += '></li>';
+        $('#featured ol').append(insertText);
+    }
+
+    $('.carousel').carousel({
+        pause: false
+    });
+
+
+
 
 }); /** Document Ready**/
 
