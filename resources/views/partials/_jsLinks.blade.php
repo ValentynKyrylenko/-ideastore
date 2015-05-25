f32236db83494865fbe622cc4159c9eea02c5a20
Valentyn <valentynmail@gmail.com>
2015-05-25 19:32:42 +0300

    fix js 3

--- resources/views/partials/_jsLinks.blade.php
+++ resources/views/partials/_jsLinks.blade.php
@@ -5,8 +5,8 @@
        <script type="text/javascript" src="{{ URL::asset('/ckeditor/ckeditor.js') }}"></script>
        <script type="text/javascript" src="{{ URL::asset('/ckeditor/adapters/jquery.js') }}"></script>
        <!------------------------------------------------------------------------------------------------->
-       <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-       <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+       {{--<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>--}}
+       {{--<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>--}}
   <script>
                    $(function(){
                    "use strict";
