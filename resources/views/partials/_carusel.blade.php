25e485fe185e5aab74699bb49277375448a06eec
Valentyn <valentynmail@gmail.com>
2015-05-25 19:01:17 +0300

    corusel not done2

--- resources/views/partials/_carusel.blade.php
+++ resources/views/partials/_carusel.blade.php
@@ -20,13 +20,13 @@
          </div>
 
   <!-- Left and right controls -->
-          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+          <a class="left carousel-control" href="#featured" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+
           </a>
-          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+          <a class="right carousel-control" href="#featured" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+
           </a>
  </div>
 
